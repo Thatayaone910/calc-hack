c0f4b2a08dd23490dc619af63b11a6fbfcbdc02a
Limpho <limmohjhb025@student.wethinkcode.co.za>
2026-01-20 08:25:07 +0200

    Added a piece of code with only variables

--- python calculator.py
+++ python calculator.py
@@ -1,1 +1,14 @@
+history = []
 
+
+while True:
+    user_input = input("Enter equation: ")
+
+    if user_input == "exit":
+        break
+
+    parts = user_input.split()
+
+    num1 = float(parts[0])
+    operator = parts[1]
+    num2 = float(parts[2])
