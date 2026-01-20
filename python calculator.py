913262fdd64b3eb9335e4b66afe77ceea9ca7957
Limpho <limmohjhb025@student.wethinkcode.co.za>
2026-01-20 09:49:19 +0200

    Uninished but working calculator that can calculator only two numbers and not more than that

--- python calculator.py
+++ python calculator.py
@@ -9,6 +9,26 @@ while True:
 
     parts = user_input.split()
 
+    if len(parts) != 3:
+        print("Format must be: Number Space Operator Space Number")
+        continue
+
     num1 = float(parts[0])
     operator = parts[1]
     num2 = float(parts[2])
+
+    
+    
+    if operator == "+":
+        print(num1 + num2)
+    
+    elif operator == "-":
+        print(num1 - num2)
+    
+    elif operator == "*":
+        print(num1 * num2)
+
+    elif operator == "/":
+        print(num1 / num2)
+    else:
+        print("Inavild Operator")
