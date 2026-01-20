a1dbfb7e52de956ca5ae83a4a49771e6a6764a8c
Yolandar Madonsela <yomadjhb025@student.wethinkcode.co.za>
2026-01-20 10:57:01 +0200

    modified to allow for a longer expression and consider BODMAS

--- python calculator.py
+++ python calculator.py
@@ -1,34 +1,13 @@
 history = []
 
-
 while True:
     user_input = input("Enter equation: ")
 
     if user_input == "exit":
         break
-
-    parts = user_input.split()
-
-    if len(parts) != 3:
-        print("Format must be: Number Space Operator Space Number")
-        continue
-
-    num1 = float(parts[0])
-    operator = parts[1]
-    num2 = float(parts[2])
-
-    
-    
-    if operator == "+":
-        print(num1 + num2)
-    
-    elif operator == "-":
-        print(num1 - num2)
-    
-    elif operator == "*":
-        print(num1 * num2)
-
-    elif operator == "/":
-        print(num1 / num2)
-    else:
+       
+    try:
+        result = eval(user_input)
+        print(result)
+    except:
         print("Inavild Operator")
