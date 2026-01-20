94dfd7ec0aeb40badfd4e70a6335101bbe4f5157
wtc <siviljhb025@student.wethinkcode.co.za>
2026-01-20 10:18:32 +0200

    implemented the whole code but my fellow members will add on

--- python calculator.py
+++ python calculator.py
@@ -1,1 +1,48 @@
+history = []
+
+while True:
+    user_input = input("select an operator or an action...(+ - * /  history exit): ")
+    if user_input == "exit":
+        print("Goodbye!")
+        break
+
+    elif user_input == "history":
+        if not history:
+            print("No history yet")
+    
+        else:
+            for item in history:
+                print(item)
+    
+    elif user_input in ["+", "-", "*", "/"]:
+        num1 = float(input("type your first number here: "))
+        num2 = float(input("type your second number here: "))
+
+        if user_input == "+" :
+            result = (num1 + num2)
+
+        elif user_input == "-":
+            result = (num1 - num2)
+    
+        elif user_input == "*":
+            result = (num1 * num2)
+
+        elif user_input == "/":
+            result = (num1 / num2)
+            
+        print(result)
+        history.append(f"{num1} {user_input} {num2} = {result}")
+    
+    else:
+        print("Invalid entry,please try again")
+    
+
+
+    
+
+
+    
+    
+
+
 
